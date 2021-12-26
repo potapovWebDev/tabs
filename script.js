8b7544505e53603ba4349d288fc58a668f4ad7ad
Максим <PotapovWebDev@gmail.com>
2021-12-26 18:36:21 +0300

    makets tabs

---
+++ script.js
@@ -0,0 +1,35 @@
+const tabs = document.querySelectorAll('.tabheader__item'),
+      tabsContent = document.querySelectorAll('.tab__content'),
+      tabsParent = document.querySelector('.tabheader__items');
+
+      function hideTabsContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none'
+          });
+
+          tabs.forEach(item => {
+              item.classList.remove('tabheader__item_active')
+          });
+      }
+
+      function showTabsContent(i = 0) {
+          tabsContent[i].style.display = 'block';
+
+          tabs[i].classList.add('tabheader__item_active')
+      }
+
+      hideTabsContent();
+      showTabsContent();
+
+      tabsParent.addEventListener('click', (event) => {
+          const target = event.target;
+
+          if(target && target.classList.contains('tabheader__item')) {
+              tabs.forEach((item, i) => {
+                  if(target == item) {
+                    hideTabsContent();
+                    showTabsContent(i);
+                  }
+              });
+          }
+      });
